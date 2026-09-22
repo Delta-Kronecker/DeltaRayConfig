d734924b327c193a14f897b5817115b79b995010
Ahmad <diracdeltain0@gmail.com>
2026-09-22 18:29:23 +0330

    Update process.py

--- process.py
+++ process.py
@@ -3,6 +3,7 @@ import urllib.parse
 
 urls = [
     "https://raw.githubusercontent.com/patterniha/Free-Configs/main/configs.txt",
+    "https://github.com/Delta-Kronecker/WARP-Config/raw/refs/heads/main/ALL.txt", 
     "https://github.com/Delta-Kronecker/DeltaRayConfig/raw/refs/heads/main/tested.tt",
     "https://github.com/Delta-Kronecker/V2ray-Config/raw/refs/heads/main/config/patt/all.tx",
 ]
